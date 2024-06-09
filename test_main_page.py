697548db6c45e8def8497b2f1b262cbd1173abed
User <ivanast-qa@gmail.com>
2024-06-10 00:33:18 +0300

    create base_page in pages

--- test_main_page.py
+++ test_main_page.py
@@ -3,10 +3,16 @@ import time
 import pytest
 from selenium.webdriver.common.by import By
 
-def test_guest_can_go_to_login_page(browser):
-    site_link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(site_link)
+
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
     time.sleep(3)
+
+def test_guest_can_go_to_login_page(browser):
+    site_link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(site_link)
+    go_to_login_page(browser)
+
+
 
