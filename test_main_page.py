672fb39c14b0f5a716e640d91d7d7e140a74c262
User <ivanast-qa@gmail.com>
2024-06-10 01:07:05 +0300

    fix base_page in pages and create main_page plus first test

--- test_main_page.py
+++ test_main_page.py
@@ -1,18 +1,13 @@
 import time
-
 import pytest
 from selenium.webdriver.common.by import By
-
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-    time.sleep(3)
-
+from .pages.main_page import MainPage
 def test_guest_can_go_to_login_page(browser):
     site_link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(site_link)
-    go_to_login_page(browser)
+    page = MainPage(browser, site_link)
+    page.open()
+    page.go_to_login_page()
+    time.sleep(4)
 
 
 
